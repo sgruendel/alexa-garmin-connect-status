e320740a32ca4a67607106a8fa7fab5161f4177f
Stefan Gründel <stefan.gruendel@gmail.com>
2020-04-29 06:45:27 +0200

    Handle duplicate status items.

--- src/garmin-connect-status.js
+++ src/garmin-connect-status.js
@@ -15,14 +15,19 @@ function parseServiceDiv(i, div) {
     return { item: item, statusReasons: statusReasons };
 }
 
+function divReducer(uniques, div) {
+    const isDuplicate = uniques.find(uniqueDiv => uniqueDiv.item === div.item);
+    return isDuplicate ? uniques : [...uniques, div];
+}
+
 var exports = module.exports = {};
 
 exports.parseBody = (body) => {
     const $ = cheerio.load(body);
 
-    const green = $('div.service.green').map(parseServiceDiv).toArray();
-    const yellow = $('div.service.yellow').map(parseServiceDiv).toArray();
-    const red = $('div.service.red').map(parseServiceDiv).toArray();
+    const green = $('div.service.green').map(parseServiceDiv).toArray().reduce(divReducer, []);
+    const yellow = $('div.service.yellow').map(parseServiceDiv).toArray().reduce(divReducer, []);
+    const red = $('div.service.red').map(parseServiceDiv).toArray().reduce(divReducer, []);
     return { green: green, yellow: yellow, red: red };
 };
 
